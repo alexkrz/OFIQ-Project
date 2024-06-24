7df79a8d490671b98bdb8d66d51a39f2fabe0732
samuel0x <samusechzehn@icloud.com>
2024-06-24 18:43:56 +0200

    still onnx import error

--- OFIQlib/src/OFIQImpl.cpp
+++ OFIQlib/src/OFIQImpl.cpp
@@ -350,6 +350,176 @@ void visualizeLandmarkRegion(Session& session)
     previewWindow("Face Landmark Region", layered);
 }
 
+static inline float sigmoid_x(float x)
+{
+	return static_cast<float>(1.f / (1.f + exp(-x)));
+}
+
+cv::Mat YOLOv8resizeImage(cv::Mat srcimg, int *newh, int *neww, int *padh, int *padw)
+{
+	int srch = srcimg.rows, srcw = srcimg.cols;
+	*newh = 640;
+	*neww = 640;
+	cv::Mat dstimg;
+	if (srch != srcw) {
+		float hw_scale = (float)srch / srcw;
+		if (hw_scale > 1) {
+			*newh = 640;
+			*neww = int(640 / hw_scale);
+			resize(srcimg, dstimg, cv::Size(*neww, *newh), cv::INTER_AREA);
+			*padw = int((640 - *neww) * 0.5);
+			copyMakeBorder(dstimg, dstimg, 0, 0, *padw, 640 - *neww - *padw, cv::BORDER_CONSTANT, 0);
+		}
+		else {
+			*newh = (int)640 * hw_scale;
+			*neww = 640;
+			resize(srcimg, dstimg, cv::Size(*neww, *newh), cv::INTER_AREA);
+			*padh = (int)(640 - *newh) * 0.5;
+			copyMakeBorder(dstimg, dstimg, *padh, 640 - *newh - *padh, 0, 0, cv::BORDER_CONSTANT, 0);
+		}
+	}
+	else {
+		resize(srcimg, dstimg, cv::Size(*neww, *newh), cv::INTER_AREA);
+	}
+	return dstimg;
+}
+
+void YOLOv8drawResults(float conf, int left, int top, int right, int bottom, cv::Mat& frame, std::vector<cv::Point> landmark)   // Draw the predicted bounding box
+{
+	//Draw a rectangle displaying the bounding box
+	cv::rectangle(frame, cv::Point(left, top), cv::Point(right, bottom), cv::Scalar(0, 0, 255), 3);
+
+	//Get the label for the class name and its confidence
+	std::string label = cv::format("face:%.2f", conf);
+
+	putText(frame, label, cv::Point(left, top-5), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 255, 0), 2);
+	for (int i = 0; i < 5; i++)
+	{
+		circle(frame, landmark[i], 4, cv::Scalar(0, 255, 0), -1);
+	}
+}
+
+void YOLOv8softMax(const float* x, float* y, int length)
+{
+	float sum = 0;
+	int i = 0;
+	for (i = 0; i < length; i++)
+	{
+		y[i] = exp(x[i]);
+		sum += y[i];
+	}
+	for (i = 0; i < length; i++)
+	{
+		y[i] /= sum;
+	}
+}
+
+void YOLOv8evalResults(cv::Mat out, std::vector<cv::Rect>& boxes, std::vector<float>& confidences, std::vector<std::vector<cv::Point>>& landmarks, int imgh,int imgw, float ratioh, float ratiow, int padh, int padw)
+{
+	const int feat_h = out.size[2];
+	const int feat_w = out.size[3];
+	std::cout << out.size[1] << "," << out.size[2] << "," << out.size[3] << std::endl;
+	const int stride = (int)ceil((float)640 / feat_h);
+	const int area = feat_h * feat_w;
+	float* ptr = (float*)out.data;
+	float* ptr_cls = ptr + area * 16 * 4;
+	float* ptr_kp = ptr + area * (16 * 4 + 1);
+
+	for (int i = 0; i < feat_h; i++)
+	{
+		for (int j = 0; j < feat_w; j++)
+		{
+			const int index = i * feat_w + j;
+			int cls_id = -1;
+			float max_conf = -10000;
+			for (int k = 0; k < 1; k++)
+			{
+				float conf = ptr_cls[k*area + index];
+				if (conf > max_conf)
+				{
+					max_conf = conf;
+					cls_id = k;
+				}
+			}
+			float box_prob = sigmoid_x(max_conf);
+			if (box_prob > 0.45)
+			{
+				float pred_ltrb[4];
+				float* dfl_value = new float[16];
+				float* dfl_softmax = new float[16];
+				for (int k = 0; k < 4; k++)
+				{
+					for (int n = 0; n < 16; n++)
+					{
+						dfl_value[n] = ptr[(k*16 + n)*area + index];
+					}
+					YOLOv8softMax(dfl_value, dfl_softmax, 16);
+
+					float dis = 0.f;
+					for (int n = 0; n < 16; n++)
+					{
+						dis += n * dfl_softmax[n];
+					}
+
+					pred_ltrb[k] = dis * stride;
+				}
+				float cx = (j + 0.5f)*stride;
+				float cy = (i + 0.5f)*stride;
+				float xmin = std::max((cx - pred_ltrb[0] - padw)*ratiow, 0.f);
+				float ymin = std::max((cy - pred_ltrb[1] - padh)*ratioh, 0.f);
+				float xmax = std::min((cx + pred_ltrb[2] - padw)*ratiow, float(imgw - 1));
+				float ymax = std::min((cy + pred_ltrb[3] - padh)*ratioh, float(imgh - 1));
+				cv::Rect box = cv::Rect(int(xmin), int(ymin), int(xmax - xmin), int(ymax - ymin));
+				boxes.push_back(box);
+				confidences.push_back(box_prob);
+
+				std::vector<cv::Point> kpts(5);
+				for (int k = 0; k < 5; k++)
+				{
+					float x = ((ptr_kp[(k * 3)*area + index] * 2 + j) * stride - padw) * ratiow;
+					float y = ((ptr_kp[(k * 3 + 1)*area + index] * 2 + i) * stride - padh) * ratioh;
+
+					kpts[k] = cv::Point(int(x), int(y));
+				}
+				landmarks.push_back(kpts);
+			}
+		}
+	}
+}
+
+void YOLOv8detect(cv::Mat& srcimg, cv::dnn::Net net)
+{
+	int newh = 0, neww = 0, padh = 0, padw = 0;
+	cv::Mat dst = YOLOv8resizeImage(srcimg, &newh, &neww, &padh, &padw);
+	cv::Mat blob;
+	cv::dnn::blobFromImage(dst, blob, 1 / 255.0, cv::Size(640, 640), cv::Scalar(0, 0, 0), true, false);
+	net.setInput(blob);
+	std::vector<cv::Mat> outs;
+	
+	net.forward(outs, net.getUnconnectedOutLayersNames());
+
+	//generate proposals
+	std::vector<cv::Rect> boxes;
+	std::vector<float> confidences;
+	std:: vector<std::vector<cv::Point>> landmarks;
+	float ratioh = (float)srcimg.rows / newh, ratiow = (float)srcimg.cols / neww;
+
+	YOLOv8evalResults(outs[0], boxes, confidences, landmarks, srcimg.rows, srcimg.cols, ratioh, ratiow, padh, padw);
+	YOLOv8evalResults(outs[1], boxes, confidences, landmarks, srcimg.rows, srcimg.cols, ratioh, ratiow, padh, padw);
+	YOLOv8evalResults(outs[2], boxes, confidences, landmarks, srcimg.rows, srcimg.cols, ratioh, ratiow, padh, padw);
+
+	// Perform non maximum suppression to eliminate redundant overlapping boxes with
+	// lower confidences
+	std::vector<int> indices;
+	cv::dnn::NMSBoxes(boxes, confidences, 0.45f, 0.5f, indices);
+	for (size_t i = 0; i < indices.size(); ++i)
+	{
+		int idx = indices[i];
+		cv::Rect box = boxes[idx];
+		YOLOv8drawResults(confidences[idx], box.x, box.y, box.x + box.width, box.y + box.height, srcimg, landmarks[idx]);
+	}
+}
+
 void OFIQImpl::performPreprocessing(Session& session, const std::string& detector)
 {
     if(detector == "" || detector == "ssd")
@@ -441,7 +611,9 @@ void OFIQImpl::performPreprocessing(Session& session, const std::string& detecto
     }
     else
     {
-        
+        cv::Mat srcimg = copyToCvImage(session.image());
+        cv::dnn::Net net = cv::dnn::readNet("/home/samuel/vc/OFIQ-Project/OFIQ-Project/data/models/face_detection/yolov8n-face.onnx");
+        YOLOv8detect(srcimg, net);
     }
 
 }
